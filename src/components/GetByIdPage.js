6417a0e0461db2dba9fee2ac9992628e6fc0118f
ShrineetaKurud <shrineeta.kurud@gmail.com>
2022-09-28 18:05:42 +0530

    Table Changes

--- src/components/GetByIdPage.js
+++ src/components/GetByIdPage.js
@@ -45,17 +45,20 @@ export default function GetByIdPage() {
     return (
 
         <div>
+            <div>
 
             {/*Heading*/}
 
             <h3 class="text-center text-white text-lg pt-2"><b>Search Logs</b></h3>
             <hr class="my-5 h-px bg-gray-200 border-0 dark:bg-gray-700"></hr>
 
+            
+
             {/*Input Section*/}
 
-            <div class="w-full max-w-full ">
+            <div class="w-full flex flex-col justify-center content-center max-w-full ">
 
-                <div class="flex flex-row pt-5">
+                <div class="flex flex-row justify-center content-center pt-5">
 
                     <div>
                         <input type="text" id="name" onChange={handleId} value={id} class="bg-gray-50 border m-5 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder=" Enter Keyword" required="">
@@ -74,31 +77,35 @@ export default function GetByIdPage() {
 
                 </div>
 
-                <button onClick={doGet} class="text-white bg-blue-700 hover:bg-blue-800 w-1/6 ml-5 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-1 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Search</button>
-                <button onClick={doReset} class="text-white bg-blue-700 hover:bg-blue-800 w-1/6 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-1 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Reset</button>
-
+                <div class="flex justify-center content-center">
+                <button onClick={doGet} class="text-white bg-blue-700 hover:bg-blue-800 w-28 ml-5 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-1 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Search</button>
+                <button onClick={doReset} class="text-white bg-blue-700 hover:bg-blue-800 w-28 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-1 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Reset</button>
+                </div>
             </div>
 
+        </div>
 
-            {/*Table for showing details of selected mobile plan id */}
 
-            <div class=" mt-10 ml-5 overflow-x-auto relative shadow-md mb-20">
+        {/*Table for showing details of selected mobile plan id */}
 
-                <table class="w-full text-sm text-left text-gray-800 dark:text-gray-400 ">
+        {logs &&
+                <div class="min-w-96 p-10">
+
+                <table class="min-w-96 text-left text-sm text-left text-gray-800 dark:text-gray-400 ">
 
                     <thead class="text-xs text-white bg-slate-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
-                            <th scope="col" class="py-3 pl-2">
+                            <th scope="col" class="p-4 ">
                                 Sr.No
                             </th>
-                            <th scope="col" class="py-3 px-20">
+                            <th scope="col" class="p-4">
+                                Operation
+                            </th>
+                            <th scope="col" class="p-4 ">
                                 Entity JSON
                             </th>
-                            <th scope="col" class="py-3 ">
+                            <th scope="col" class="p-4 ">
                                 Modification Date
-                            </th>
-                            <th scope="col" class="py-3 ">
-                                Operation
                             </th>
                         </tr>
                     </thead>
@@ -108,16 +115,16 @@ export default function GetByIdPage() {
                             logs && logs.map((log) => {
                                 return (
                                     <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                        <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        <th scope="row" class="p-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                             {log.id}
                                         </th>
-                                        <td class="py-4 px-6">
+                                        <td class="p-4 ">
                                             {log.operationType}
                                         </td>
-                                        <td class="py-4 px-6">
+                                        <td class="p-4 ">
                                             {log.entityJson}
                                         </td>
-                                        <td class="py-4 px-6">
+                                        <td class="p-4 ">
                                             {log.modificationDate}
                                         </td>
 
@@ -129,7 +136,7 @@ export default function GetByIdPage() {
 
                 </table>
 
-            </div>
+            </div>}
 
         </div>
     )
